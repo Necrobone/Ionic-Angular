911e58359fadb2763b61782f949e7a4e83648c87
Juan Jesús Gutiérrez Romero <necrobone@hotmail.com>
2019-11-17 20:31:39 +0100

    Side Menu

--- src/app/places/search/search.page.ts
+++ src/app/places/search/search.page.ts
@@ -1,19 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PlacesService} from '../places.service';
 import {Place} from '../place.model';
+import {MenuController} from '@ionic/angular';
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.page.html',
-  styleUrls: ['./search.page.scss'],
+    selector: 'app-search',
+    templateUrl: './search.page.html',
+    styleUrls: ['./search.page.scss'],
 })
+
 export class SearchPage implements OnInit {
-  loadedPlaces: Place[];
+    loadedPlaces: Place[];
 
-  constructor(private placesService: PlacesService) { }
+    constructor(private placesService: PlacesService, private menuController: MenuController) {}
 
-  ngOnInit() {
-    this.loadedPlaces = this.placesService.places;
-  }
+    ngOnInit() {
+        this.loadedPlaces = this.placesService.places;
+    }
 
+    onOpenMenu() {
+        this.menuController.toggle();
+    }
 }
