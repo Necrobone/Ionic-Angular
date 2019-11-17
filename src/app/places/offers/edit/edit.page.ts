ea02562060d6e9c04cb91dd5af3d76a79f35dcb9
Juan Jesús Gutiérrez Romero <necrobone@hotmail.com>
2019-11-17 16:06:30 +0100

    Adding ID to view places pages

--- src/app/places/offers/edit/edit.page.ts
+++ src/app/places/offers/edit/edit.page.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Place} from '../../place.model';
+import {ActivatedRoute} from '@angular/router';
+import {NavController} from '@ionic/angular';
+import {PlacesService} from '../../places.service';
 
 @Component({
-  selector: 'app-edit',
-  templateUrl: './edit.page.html',
-  styleUrls: ['./edit.page.scss'],
+    selector: 'app-edit',
+    templateUrl: './edit.page.html',
+    styleUrls: ['./edit.page.scss'],
 })
 export class EditPage implements OnInit {
+    place: Place;
 
-  constructor() { }
+    constructor(private route: ActivatedRoute, private navCtrl: NavController, private placesService: PlacesService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.route.paramMap.subscribe(paramMap => {
+            if (!paramMap.has('placeId')) {
+                this.navCtrl.navigateBack('/places/tabs/offers');
+                return;
+            }
+
+            this.place = this.placesService.getPlace(paramMap.get('placeId'));
+        });
+    }
 
 }
--- src/app/places/search/view/view.page.ts
+++ src/app/places/search/view/view.page.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 import {NavController} from '@ionic/angular';
+import {Place} from '../../place.model';
+import {PlacesService} from '../../places.service';
 
 @Component({
-  selector: 'app-view',
-  templateUrl: './view.page.html',
-  styleUrls: ['./view.page.scss'],
+    selector: 'app-view',
+    templateUrl: './view.page.html',
+    styleUrls: ['./view.page.scss'],
 })
 export class ViewPage implements OnInit {
+    place: Place;
 
-  constructor(private router: Router, private navCtrl: NavController) { }
+    constructor(private route: ActivatedRoute, private navCtrl: NavController, private placesService: PlacesService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.route.paramMap.subscribe(paramMap => {
+            if (!paramMap.has('placeId')) {
+                this.navCtrl.navigateBack('/places/tabs/search');
+                return;
+            }
 
-  onBookPlace() {
-    this.navCtrl.navigateBack('/places/tabs/search');
-  }
+            this.place = this.placesService.getPlace(paramMap.get('placeId'));
+        });
+    }
+
+    onBookPlace() {
+        this.navCtrl.navigateBack('/places/tabs/search');
+    }
 }
