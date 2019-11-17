57c8a9a8c8541421589574f1cc225cfe25114985
Juan Jesús Gutiérrez Romero <necrobone@hotmail.com>
2019-11-17 15:48:20 +0100

    Offer list, offer page, add offer and edit offer basic

--- src/app/places/offers/bookings/bookings.page.ts
+++ src/app/places/offers/bookings/bookings.page.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Place} from '../../place.model';
+import {NavController} from '@ionic/angular';
+import {PlacesService} from '../../places.service';
 
 @Component({
-  selector: 'app-bookings',
-  templateUrl: './bookings.page.html',
-  styleUrls: ['./bookings.page.scss'],
+    selector: 'app-bookings',
+    templateUrl: './bookings.page.html',
+    styleUrls: ['./bookings.page.scss'],
 })
 export class BookingsPage implements OnInit {
+    place: Place;
 
-  constructor() { }
+    constructor(private route: ActivatedRoute, private navCtrl: NavController, private placesService: PlacesService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.route.paramMap.subscribe(paramMap => {
+            if (!paramMap.has('placeId')) {
+                this.navCtrl.navigateBack('/places/tabs/offers');
+                return;
+            }
+
+            this.place = this.placesService.getPlace(paramMap.get('placeId'));
+        });
+    }
 
 }
