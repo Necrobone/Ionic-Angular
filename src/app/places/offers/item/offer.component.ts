9457064aff2b596aec5f6648b7c23eadad03d6bc
Juan Jesús Gutiérrez Romero <necrobone@hotmail.com>
2019-11-30 15:33:28 +0100

    Styling examples and custom component

---
+++ src/app/places/offers/item/offer.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Place} from '../../place.model';
+
+@Component({
+  selector: 'app-offer',
+  templateUrl: './offer.component.html',
+  styleUrls: ['./offer.component.scss'],
+})
+export class OfferComponent implements OnInit {
+  @Input() offer: Place;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  getDummyDate() {
+    return new Date();
+  }
+}
