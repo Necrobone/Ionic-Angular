cfa5083b43540d12eb9452cd9a112c69c11fe67e
Juan Jesús Gutiérrez Romero <necrobone@hotmail.com>
2019-11-30 13:00:54 +0100

    Segments

--- src/app/places/search/search.page.ts
+++ src/app/places/search/search.page.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {PlacesService} from '../places.service';
 import {Place} from '../place.model';
 import {MenuController} from '@ionic/angular';
+import {SegmentChangeEventDetail} from '@ionic/core';
 
 @Component({
     selector: 'app-search',
@@ -11,14 +12,20 @@ import {MenuController} from '@ionic/angular';
 
 export class SearchPage implements OnInit {
     loadedPlaces: Place[];
+    listedLoadedPlaces: Place[];
 
     constructor(private placesService: PlacesService, private menuController: MenuController) {}
 
     ngOnInit() {
         this.loadedPlaces = this.placesService.places;
+        this.listedLoadedPlaces = this.loadedPlaces.slice(1);
     }
 
     onOpenMenu() {
         this.menuController.toggle();
+    }
+
+    onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
+        console.log(event.detail);
     }
 }
