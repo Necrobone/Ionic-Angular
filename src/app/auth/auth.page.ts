910308359626dcf237f33472d57c9fceeb49e54f
Juan Jesús Gutiérrez Romero <necrobone@hotmail.com>
2019-12-01 16:44:36 +0100

    Basic form

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {AuthService} from './auth.service';
 import {Router} from '@angular/router';
 import {LoadingController} from '@ionic/angular';
+import {NgForm} from '@angular/forms';
 
 @Component({
     selector: 'app-auth',
@@ -9,14 +10,13 @@ import {LoadingController} from '@ionic/angular';
     styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
-    isLoading = false;
+    isLogin = true;
 
     constructor(private authService: AuthService, private router: Router, private loadingController: LoadingController) {}
 
     ngOnInit() {}
 
     onLogin() {
-        this.isLoading = true;
         this.authService.login();
         this.loadingController.create({
             keyboardClose: true,
@@ -24,10 +24,30 @@ export class AuthPage implements OnInit {
         }).then(loadingEl => {
             loadingEl.present();
             setTimeout(() => {
-                this.isLoading = false;
                 loadingEl.dismiss();
                 this.router.navigateByUrl('/places/tabs/search');
             }, 1500);
         });
+    }
+
+    onSubmit(form: NgForm) {
+        if (!form.valid) {
+            return;
+        }
+
+        const email = form.value.email;
+        const password = form.value.password;
+
+        console.log(email, password);
+
+        if (this.isLogin) {
+            // send a request to login servers
+        } else {
+            // send a request to signup servers
+        }
+    }
+
+    onSwitchAuthMode() {
+        this.isLogin = !this.isLogin;
     }
 }
