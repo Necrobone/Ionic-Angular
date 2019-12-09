90fb39846d8801d1a7868321db20768a6326ad6e
Necrobone <necrobone@hotmail.com>
2019-12-09 11:59:44 +0100

    Reactive form

--- src/app/places/offers/add/add.page.ts
+++ src/app/places/offers/add/add.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-add',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add.page.scss'],
 })
 export class AddPage implements OnInit {
+  form: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      description: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(180)]
+      }),
+      price: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.min(1)]
+      }),
+      dateFrom: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      dateTo: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      })
+    });
+  }
+
+  onCreateOffer() {
+    if (!this.form.valid) {
+      return;
+    }
   }
 
 }
--- src/app/places/offers/edit/edit.page.ts
+++ src/app/places/offers/edit/edit.page.ts
@@ -3,6 +3,7 @@ import {Place} from '../../place.model';
 import {ActivatedRoute} from '@angular/router';
 import {NavController} from '@ionic/angular';
 import {PlacesService} from '../../places.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
     selector: 'app-edit',
@@ -11,6 +12,7 @@ import {PlacesService} from '../../places.service';
 })
 export class EditPage implements OnInit {
     place: Place;
+    form: FormGroup;
 
     constructor(private route: ActivatedRoute, private navCtrl: NavController, private placesService: PlacesService) {
     }
@@ -23,7 +25,23 @@ export class EditPage implements OnInit {
             }
 
             this.place = this.placesService.getPlace(paramMap.get('placeId'));
+            this.form = new FormGroup({
+                title: new FormControl(this.place.title, {
+                    updateOn: 'blur',
+                    validators: [Validators.required]
+                }),
+                description: new FormControl(this.place.description, {
+                    updateOn: 'blur',
+                    validators: [Validators.required, Validators.maxLength(180)]
+                })
+            });
         });
     }
 
+    onEditOffer() {
+        if (!this.form.valid) {
+            return;
+        }
+        console.log(this.form);
+    }
 }
